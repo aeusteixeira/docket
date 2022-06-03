00b4905bbf60ed8012269f7f2576550d85d05abb
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2022-06-03 09:58:23 -0300

    Ajustado Auth

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WelcomeController extends Controller
 {
@@ -15,8 +16,6 @@ class WelcomeController extends Controller
 
     public function configure(Request $request)
     {
-
-
         Auth::login($user);
 
         return redirect()->route('dashboard.index');
